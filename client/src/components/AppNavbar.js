482623a2b4b0b9cbce6f41d500be4c5cc8e65cff
Muhammad Usman <muhammadusman0200@gmail.com>
2020-01-12 19:25:30 -0600

    Added Navbar

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+
+const AppNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">Code Mate</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="https://github.com/Usman0111/Code-Mate">
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default AppNavbar;
